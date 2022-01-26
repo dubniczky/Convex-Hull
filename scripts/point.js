85ffd40e7a86c232ac3222aa8ec8341be94e28e3
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-26 23:58:08 +0100

    Updated point js to ES12 syntax

--- scripts/point.js
+++ scripts/point.js
@@ -1,49 +1,52 @@
-//Required globals:
-//   pointRadius: integer (>=0)
-//   rad: float
-
 class Point
 {
-   constructor(pos, col, rad)
-   {
-      this.pos = pos;
-      this.col = col;
-      this.rad = rad;
-   }
+    // Fields
+    pos = null
+    col = null
+    rad = null
 
-   draw(col)
-   {
-      if (col == null) fill(this.col);
-      else fill(col);
-      
-      circle(this.pos.x, this.pos.y, this.rad);
-   }
+    // Constructor
+    constructor(pos, col, rad) {
+        this.pos = pos
+        this.col = col
+        this.rad = rad
+    }
 
-   distance(point)
-   {
-      return Math.sqrt(point.x**2 + point.y**2);
-   }
-   theta(point)
-   {
-      let a = this.pos.x - point.pos.x;
-      let b = this.pos.y - point.pos.y;
+    // Drawing methods
 
-      return (Math.atan2(b, a) * rad);
-   }
-   triSign(a, b)
-   {
-      return (this.pos.x - b.pos.x) * (a.pos.y - b.pos.y) - (a.pos.x - b.pos.x) * (this.pos.y - b.pos.y);
-   }
+    draw(col) {
+        if (col == null) {
+            fill(this.col)
+        }
+        else {
+            fill(col)
+        }
+        
+        circle(this.pos.x, this.pos.y, this.rad)
+    }
 
-   inTriangle(p1, p2, p3)
-   {
-      let d1 = this.triSign(p1, p2);
-      let d2 = this.triSign(p2, p3);
-      let d3 = this.triSign(p3, p1);
+    // Compute functions
 
-      let neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-      let pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    distance(point) {
+        return Math.sqrt(point.x ** 2 + point.y ** 2)
+    }
+    theta(point) {
+        let a = this.pos.x - point.pos.x
+        let b = this.pos.y - point.pos.y
 
-      return !(neg && pos);
-   }
+        return (Math.atan2(b, a) * rad)
+    }
+    triSign(a, b) {
+        return (this.pos.x - b.pos.x) * (a.pos.y - b.pos.y) - (a.pos.x - b.pos.x) * (this.pos.y - b.pos.y)
+    }
+    inTriangle(p1, p2, p3) {
+        let d1 = this.triSign(p1, p2)
+        let d2 = this.triSign(p2, p3)
+        let d3 = this.triSign(p3, p1)
+
+        let neg = (d1 < 0) || (d2 < 0) || (d3 < 0)
+        let pos = (d1 > 0) || (d2 > 0) || (d3 > 0)
+
+        return !(neg && pos)
+    }
 }
